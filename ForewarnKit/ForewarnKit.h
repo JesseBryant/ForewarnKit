4f413c17d8089af646eb91167e9be00278c132c2
IOS Directed Study <IOSDirectedStudy@apples-imac.local>
2018-01-11 15:28:52 -0600

    Initial Commit

---
+++ ForewarnKit/ForewarnKit.h
@@ -0,0 +1,19 @@
+//
+//  ForewarnKit.h
+//  ForewarnKit
+//
+//  Created by IOS Directed Study on 1/11/18.
+//  Copyright © 2018 IOS Directed Study. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ForewarnKit.
+FOUNDATION_EXPORT double ForewarnKitVersionNumber;
+
+//! Project version string for ForewarnKit.
+FOUNDATION_EXPORT const unsigned char ForewarnKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ForewarnKit/PublicHeader.h>
+
+
