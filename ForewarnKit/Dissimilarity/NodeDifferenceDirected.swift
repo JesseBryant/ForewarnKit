b84fa8f2e9120eccb602b5dfdd5fb97849fe96c4
JesseBryant <36494718+JesseBryant@users.noreply.github.com>
2018-03-22 20:51:09 -0500

    Delete NodeDifferenceDirected.swift

--- ForewarnKit/Dissimilarity/NodeDifferenceDirected.swift
+++
@@ -1,9 +0,0 @@
-//
-//  NodeDifferenceDirected.swift
-//  ForewarnKit
-//
-//  Created by IOS Directed Study on 2/9/18.
-//  Copyright © 2018 IOS Directed Study. All rights reserved.
-//
-
-import Foundation
